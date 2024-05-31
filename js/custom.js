203c11acec9902615043b808372cb684ca10d189
FrancescoCorbosiero <francesco.corbosiero00@gmail.com>
2024-05-31 21:28:09 +0200

    Home v1

--- js/custom.js
+++ js/custom.js
@@ -38,3 +38,57 @@ function myMap() {
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
+
+// Tracking
+async function getTrackingState(){
+    let trackingCodeInput = document.getElementById('tracking-code-input-id');
+    let trackingStateDiv = document.getElementById('tracking-state-div-id');
+    let trackingStateTxt = document.getElementById('tracking-state-txt-id');
+    let trackingH2 = document.getElementById('tracking-h2-id');
+
+    let codice = trackingCodeInput.value;
+    if(!codice){
+        trackingStateDiv.hidden = true;
+    } else {
+
+        try{
+            let status = await getTrackingStateRestCall(codice);
+            trackingH2.innerHTML = 'Stato spedizione N. ' + codice;
+            trackingStateTxt.innerHTML = status;
+            trackingStateDiv.hidden = false;
+        }catch(error){
+            console.log(error);
+            trackingStateTxt.innerHTML = 'Codice non valido';
+            trackingStateDiv.hidden = false;
+        }
+    }
+}
+
+async function getTrackingStateRestCall(codice){
+    let url = 'http://uat-sanchez-logistica.eu-north-1.elasticbeanstalk.com/mono/api/order/trace/state';
+    let req = {codice: codice}
+
+    let response = await doPostNoAuthRestCall(url, req);
+    let responseJson = await response.json();
+    return responseJson.status;
+}
+
+async function doPostNoAuthRestCall(url, req){
+    let response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    });
+
+    let restCallFailed = response.ok == false;
+
+    if(restCallFailed){
+        throw new Error("Rest call failed");
+    }
+
+    return response;
+}
+
+document.getElementById('tracking-btn-id').onclick = () => getTrackingState();
